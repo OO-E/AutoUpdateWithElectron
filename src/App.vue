2eb818987f0db4f65326c26fe3c63ba62739e652
ozgunergen <>
2021-02-16 01:49:54 +0300

    bug fix

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App V1.0.13"/>
+    <HelloWorld msg="Welcome to Your Vue.js App V1.0.14"/>
   </div>
 </template>
 
